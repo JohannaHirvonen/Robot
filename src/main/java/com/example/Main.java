4ba6dd1003634dc4c38fa35559a240af5799f3cb
Johanna Hirvonen <johannahirvonen@live.se>
2024-11-14 16:42:27 +0100

    first commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,16 @@
+package com.example;
+
+/*
+ * 1. FloorSize: input two digits, witdth and dept
+ * 2. StartingPosition: input two digits (width and dept) and a direction: N, E, S, W
+ * 3. Move: input a string of letters (R, L, F) for commanding the robot to turn Right, Left, or move Forward
+ *      If the robot moves outside the floor: exit with error
+ * 4. Report: After the string array is iterated the robot reports its position as width, dept, direction
+ * 
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Robot!");
+    }
+}
